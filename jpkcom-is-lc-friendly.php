62c7105ed35c8c0a102e526127d5041a00b58182
Jean Pierre Kolb <jpk@jpkc.com>
2026-07-29 09:30:27 +0200

    v1.0.8: tested up to WordPress 7.1, minimum 6.9 throughout
    - Plugin header and README now declare Tested up to: 7.1
    - The bundled updater bailed out below WordPress 6.8 while the plugin header has
      required 6.9 for several releases, so the check could never fire on a supported
      installation. Both now say 6.9.
    - Release manifest fallbacks for requires/tested raised to 6.9 / 7.1

--- jpkcom-is-lc-friendly.php
+++ jpkcom-is-lc-friendly.php
@@ -3,16 +3,16 @@
 Plugin Name: JPKCom Theme is LiveCanvas friendly
 Plugin URI: https://github.com/JPKCom/jpkcom-is-lc-friendly
 Description: Enables lc_theme_is_livecanvas_friendly().
-Version: 1.0.7
+Version: 1.0.8
 Author: Jean Pierre Kolb <jpk@jpkc.com>
 Author URI: https://www.jpkc.com
 Contributors: JPKCom
 Tags: LiveCanvas, Bootstrap, Theme
 Requires Plugins: livecanvas
 Requires at least: 6.9
-Tested up to: 7.0
+Tested up to: 7.1
 Requires PHP: 8.3
-Stable tag: 1.0.7
+Stable tag: 1.0.8
 License: GPL-2.0-or-later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 */
@@ -30,7 +30,7 @@ if ( ! defined( constant_name: 'WPINC' ) ) {
  * @since 1.0.4
  */
 if ( ! defined( 'JPKCOM_IS_LC_FRIENDLY_VERSION' ) ) {
-    define( 'JPKCOM_IS_LC_FRIENDLY_VERSION', '1.0.7' );
+    define( 'JPKCOM_IS_LC_FRIENDLY_VERSION', '1.0.8' );
 }
 
 
